31e0e09aa3f93cae5046c809c6f60db214318b34
muhammadniko <mn.muhammadniko@gmail.com>
2023-02-11 21:28:31 +0800

    Add routes for About page

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('/hello', function() {
 	return view('hello');
+});
+
+Route::get('/about', function() {
+	return "This for about page";
 });
